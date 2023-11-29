da82f6fdfa52b1e7fd522bb1d9df2247f4aa1562
iammakena@gmail.com <root@MAKENA>
2023-11-29 10:48:51 +0300

    Ascii, ord, chr, range

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
     print(chr(i), end="")
-'''
-ord function in python returns the integer representation
-of a character according to the Unicode std.
-chr function will then convert each integer to back to its corresponding char
-to remove the newline added automatically by print, use the end parameter.
-'''
