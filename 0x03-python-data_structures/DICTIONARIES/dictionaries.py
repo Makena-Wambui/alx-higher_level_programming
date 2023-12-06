56fd67230fc00b4a89516d370d553b21ba7e16a7
iammakena@gmail.com <root@MAKENA>
2023-12-06 10:58:25 +0300

    Creating new entries, and updating

--- 0x03-python-data_structures/DICTIONARIES/dictionaries.py
+++ 0x03-python-data_structures/DICTIONARIES/dictionaries.py
@@ -2,6 +2,11 @@
 def dictionaries():
     Personal_Info = {'Name': 'Alice', 'Home': 'Meru', 'Age': 25}
     print("The Value of the Name key is: {:s}".format(Personal_Info['Name']))
+    # print("Absent key: {:s}".format(Personal_Info['Address']))
+    Personal_Info['Address'] = '1046 Kiambu'
+    print("VaLue of Address key is {:s}".format(Personal_Info['Address']))
+    Personal_Info['Name'] = 'Makena'
+    print("{}".format(Personal_Info))
 
 
 if __name__ == '__main__':
