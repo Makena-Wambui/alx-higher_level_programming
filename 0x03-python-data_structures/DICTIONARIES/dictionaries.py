6caa0570aa512f9df29159cffa07746d34083d02
iammakena@gmail.com <root@MAKENA>
2023-12-06 10:19:10 +0300

    Dictionaries and retrieving their values

---
+++ 0x03-python-data_structures/DICTIONARIES/dictionaries.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def dictionaries():
+    Personal_Info = {'Name': 'Alice', 'Home': 'Meru', 'Age': 25}
+    print("The Value of the Name key is: {:s}".format(Personal_Info['Name']))
+
+
+if __name__ == '__main__':
+    dictionaries()
