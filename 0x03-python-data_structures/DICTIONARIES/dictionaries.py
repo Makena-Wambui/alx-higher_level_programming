70d685de6d3913ca82d374324ec6cdee0441a918
iammakena@gmail.com <root@MAKENA>
2023-12-06 11:18:09 +0300

    Using dict()

--- 0x03-python-data_structures/DICTIONARIES/dictionaries.py
+++ 0x03-python-data_structures/DICTIONARIES/dictionaries.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def dictionaries():
     Personal_Info = {'Name': 'Alice', 'Home': 'Meru', 'Age': 25}
+    Cities = dict([('Kenya', 'Nairobi'), ('Ethiopia', 'Addis Ababa'), ('UK', 'London')])
     print("The Value of the Name key is: {:s}".format(Personal_Info['Name']))
     # print("Absent key: {:s}".format(Personal_Info['Address']))
     Personal_Info['Address'] = '1046 Kiambu'
@@ -8,6 +9,10 @@ def dictionaries():
     Personal_Info['Name'] = 'Makena'
     del Personal_Info['Age']
     print("{}".format(Personal_Info))
+    print(Cities)
+    # print("{}".format(Cities[2]))
+    del Cities['UK']
+    print(Cities)
 
 
 if __name__ == '__main__':
