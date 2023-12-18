255baaf212042ffd090168be1f2a20989cecd7ca
iammakena@gmail.com <root@MAKENA>
2023-12-18 17:00:23 +0300

    First task ammended for Index error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     number = 0  # let us keep track of the number of elements being printed
-    for value in range(x):
+    for index in range(x):
         try:
-            print("{}".format(value), end="")
+            print("{}".format(my_list[index]), end="")
             number += 1
         except IndexError:  # index in the list that is out of range
-            break
-    print()
+            break  # if index error is raised the loop will break
+    print() 
     return number
