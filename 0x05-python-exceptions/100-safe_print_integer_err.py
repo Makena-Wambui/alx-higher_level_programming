e8892e0cedd667b4af8ca3c852e772c5f54cdb67
iammakena@gmail.com <root@MAKENA>
2023-12-18 23:03:17 +0300

    Printing Exceptions to std error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as Err:
+        # use the file=sys.stderr parameter to direct output
+        # to stderr instead of stdout
+        print("Exception: {}".format(Err), file=sys.stderr)
+        return False
