4391e2e68f2b3966b9ead28d6a7f15f2fcd76ad1
iammakena@gmail.com <root@MAKENA>
2023-12-24 17:47:10 +0300

    Private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class called Square."""
+class Square:
+    """This is class Square.
+    I have defined a private instance attribute size.
+    I have assigned the value of the x paremeter in the init method
+    to the size attribute.
+
+    Methods:
+        __init__
+    Attributes:
+        __size"""
+    def __init__(self, x):
+        self.__size = x
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -21,3 +21,8 @@ How does Python find the attributes of an object or class
 How to use the getattr function
 
 Write an empty class Square that defines a square.
+
+Write a class Square that defines a square by: (based on 0-square.py)
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
