4f8bf88cdb68864bcb44b2fd5194f662c2a54d5d
iammakena@gmail.com <root@MAKENA>
2023-12-24 17:27:23 +0300

    Modified doc string

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 class Square:
-    """This is an empty class called Square. It was intentionally
-    left empty for now using the pass statement."""
+    """
+    This is an empty class that represents a Square.
+
+    It was intentionally left empty
+    for now using the pass statement.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    More ststatements will be added in the future to cater for functionality
+    related to Squares such as calculating area and perimeter.
+
+    """
     pass
