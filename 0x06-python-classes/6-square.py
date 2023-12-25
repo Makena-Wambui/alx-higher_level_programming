2e131c3556e854b56380298b9b472d8d6af4f4fe
iammakena@gmail.com <root@MAKENA>
2023-12-25 16:13:11 +0300

    Changes to position setter.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,11 +45,9 @@ class Square:
         """This setter sets the value of the position attribute to
         value.
         This value must be a tuple of 2 positive integers."""
-        if type(value) is not tuple or len(value) != 2 or \
-                type(value[0]) is not int or \
-                type(value[1]) is not int or \
-                value[0] < 0 or \
-                value[1] < 0:
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError('position must be a tuple of 2 positive integers')
 
     def area(self):
