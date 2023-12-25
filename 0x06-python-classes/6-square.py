d9f31c99cd045fcd0fe1d13b3c5e54e28bf83686
iammakena@gmail.com <root@MAKENA>
2023-12-25 17:09:15 +0300

    Stupid mistake on position setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,6 +49,7 @@ class Square:
                 not all(isinstance(i, int) for i in value) or
                 not all(i >= 0 for i in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
 
     def area(self):
         """This method returns the area of the Square object."""
