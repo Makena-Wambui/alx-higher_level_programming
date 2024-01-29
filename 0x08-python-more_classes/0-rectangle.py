9753ebdd50f6fddac19c5d7e389482006d8998fa
iammakena@gmail.com <root@MAKENA>
2024-01-29 16:06:27 +0300

    Creation of an empty class, Rectangle.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This is the 0-rectangle module.
+It contains one empty class, Rectangle.
+"""
+
+
+class Rectangle:
+    """This class, Rectangle, is an empty class.
+    """
+    pass
