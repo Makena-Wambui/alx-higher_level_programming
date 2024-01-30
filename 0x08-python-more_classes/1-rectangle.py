a062cb74e4d02f958862d3e0cd356d3f56c40450
iammakena@gmail.com <root@MAKENA>
2024-01-30 11:24:15 +0300

    Initialized the attributes as public.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,8 +27,8 @@ class Rectangle:
             Width
             Height
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
