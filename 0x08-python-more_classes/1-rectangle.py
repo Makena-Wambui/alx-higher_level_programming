2030d4cfcddd5c38cefc7329e3a85768b0c5a71e
iammakena@gmail.com <root@MAKENA>
2024-01-30 11:20:43 +0300

    Fixed the order in which the instances appear in the dictionary.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,8 +27,8 @@ class Rectangle:
             Width
             Height
         """
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
