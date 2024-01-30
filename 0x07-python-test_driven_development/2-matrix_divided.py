973d9b2aff6958957517c5d706fe3cf1a1cf21bf
iammakena@gmail.com <root@MAKENA>
2024-01-30 22:42:52 +0300

    My modules and doctests in the Test Driven Developmebt Project.

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This is the 2-matrix_divided module.
+It supplies one function: the matrix_divided function.
+"""
+
+
+def matrix_divided(matrix, div):
+    """This function divides all elements of a matrix with a number, div."""
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists)" +
+                        " of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)" +
+                        " of integers/floats")
+    # each row must be of the same size:
+    length = len(matrix[0])
+    if not all(len(row) == length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    # all elements of each row must be an integer or float
+    for row in matrix:
+        if not all(isinstance(i, (int, float)) for i in row):
+            raise TypeError("matrix must be a matrix (list of lists)" +
+                            "of integers/floats")
+    # div must be an integer or float
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    # div can not be zero
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    # create a new matrix
+    new = []
+    # for each row create a new row.
+    # loop over each element in each row
+    for row in matrix:
+        new_row = []
+        for i in row:
+            # divide by div and round to two dps
+            result = round(i / div, 2)
+            # append result to new_row
+            new_row.append(result)
+        # append new rows to new matrix
+        new.append(new_row)
+    return new
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is the ===5-text_indentation module===
+It supplies one function: text_indentation.
+"""
+
+
+def text_indentation(text):
+    """Prints input text with 2 new lines after each
+    of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # let us iterate through each character in the string, string
+    for c in ".:?":
+        # replace all occurences of tht character with that character
+        # followed by two new lines.
+        text = (c + "\n\n").join([line.strip(" ") for line in text.split(c)])
+        """split the text into a list of strings,
+        using the character c as the separator.
+        iterate through each line in that list and use strip
+        to remove leading or tarailing whitespaces.
+        then join the strings back together using the character c
+        and two new lines.
+        """
+    print("{}".format(text), end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
