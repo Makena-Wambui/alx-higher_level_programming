1ef0b957d43f0f923d9b91f4d6b00a29f3bcb468
iammakena@gmail.com <root@MAKENA>
2024-02-01 05:21:52 +0300

    A static method which checks which rectangle object is bigger based on the area.

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -162,15 +162,17 @@ class Rectangle:
         Args: rect_1, rect_2
         It does not need self or cls as the first parameter.
         It returns the biggest rectangle based on the area.
+
+        rect_1 must be an instance of Rectangle
+        rect_2 must be an instance of Rectangle
         """
 
-        # rect_1 must be an instance of Rectangle
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
 
-        # rect_2 must be an instance of Rectangle
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
+
         if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
             return rect_1
         if rect_2.area() > rect_1.area():
