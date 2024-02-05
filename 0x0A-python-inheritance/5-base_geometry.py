e9ea51d3b7575ffa0c4a1bf8027125444efe08f3
iammakena@gmail.com <root@MAKENA>
2024-02-06 00:34:38 +0300

    Empy class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This is the 5-base_geometry module.
+It supplies one class: BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class called BaseGeometry.
+    """
+    pass
