be884b122339a5394d404b3c1899eccb699f22f6
iammakena@gmail.com <root@MAKENA>
2024-02-12 11:15:31 +0300

    Building on Rectangle definition with private instance attributes, width and height and properties

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+"""
+This is the 1-rectangle module.
+It supplies one class: Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This is the class Rectangle.
+
+    Methods:
+        __init__
+
+    Attributes:
+        width
+        height
+
+    Properties:
+        width
+        height
+    Property setters:
+        width.setter
+        height.setter
+
+    """
+    def __init__(self, width=0, height=0):
+        """
+        This is the Constructor method.
+        It is called each time the class is instantiated.
+        Each Rectangle object has attributes, width and height,
+        whose values are initialized to the values of the parameters,
+        width and height.
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Property width that retrieves the value of the width attribute.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Propery setter width to set the value of the
+        attribute width to value.
+        width must be an integer
+        width should not be less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Property height that retrieves the value of the height attribute.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Propery setter height to set the value of the
+        attribute height to value.
+        height must be an integer
+        height should not be less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
