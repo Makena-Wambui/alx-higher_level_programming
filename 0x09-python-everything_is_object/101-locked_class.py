ee2a5ae7d09e2187591daf15203a0fad7c4ff2f3
iammakena@gmail.com <root@MAKENA>
2024-02-14 16:13:41 +0300

    Using the __slots__ class variable

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This is the 101-locked_class module.
+It supplies one class:
+    LockedClass
+EXPLANATION:
+------------
+Slots is only used in the context of Python classes.
+It is a class variable.
+It is usually assigned a sequence
+of strings, that are variable names used by instances.
+Python reserves space for them in memory,
+and prevents creation of __dict__ and __weakref__
+attributes.
+Also prevents creation of any variables that are not declared in
+__slots__
+"""
+
+class LockedClass:
+    """
+    This is the class LockedClass.
+    No class or object attributes
+    No methods
+    Prevents the user from dynamically creating any
+    New instance attributes,
+    unless if the new instance attribute is
+    first_name.
+    """
+    __slots__ = ('first_name')
