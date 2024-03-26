1bb01e360d5527e311de65fdafa99a68ea3347ed
iammakena@gmail.com <root@MAKENA>
2024-03-27 01:24:02 +0300

    UPDATE a record ina table.

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Updates the score of Bob to 10 in the table second_table.
+-- Use UPDATE table_name SET "attribute_name" = "expression" WHERE <condition>
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
