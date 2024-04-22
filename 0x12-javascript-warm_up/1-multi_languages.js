6e6d4a3f7f8f5b3734596b68fde8cbc2e563cb16
iammakena@gmail.com <root@MAKENA>
2024-04-22 12:43:36 +0300

    Used an array to printbthree lines on the console.

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines.
+// Used an array to achieve this
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const myString of myArray) {
+  console.log(myString);
+}
