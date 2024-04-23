f2e2836186fac0dbedfb5401edcfe7395946ae1a
iammakena@gmail.com <root@MAKENA>
2024-04-23 22:14:46 +0300

    Fix semistandard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,7 +3,7 @@
 exports.converter = function (base) {
   // the converter function takes a single argument, base
   // this following function is a  closure defined using a return statement.
-  // it takes an argument number. 
+  // it takes an argument number.
   return function (number) {
     // this closure converts number to a string using the specified base.
     return number.toString(base);
