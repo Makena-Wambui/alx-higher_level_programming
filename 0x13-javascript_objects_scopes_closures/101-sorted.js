2bda301a472e736206c85a992f0f40441de2bb47
iammakena@gmail.com <root@MAKENA>
2024-04-23 23:34:26 +0300

    Fix spellings

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,13 +3,13 @@
 // Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
 
 // Let's import data from 101-data.js
-// Then extract dict property is from the imported module and assign to the variable myDict.
+// Then extract dict property from the imported module and assign to the variable myDict.
 const myDict = require('./101-data').dict;
 
 // Initialize an empty object
 const myResult = {};
 
-// This line creates an arraY containing all the keys (property names) from the dict object
+// This line creates an array containing all the keys (property names) from the dict object
 const myDictKeys = Object.keys(myDict);
 
 // This line creates an array containing all the values (property values) from the dict object
@@ -23,7 +23,7 @@ for (let i = 0; i < myDictValues.length; i++) {
   // Creates an empty array inside myResult with the key as the stringified version of myDictValues
   myResult[JSON.stringify(myDictValues[i])] = [];
 
-  // Filters the keys to find those whose corresponding value matches the current value:
+  // Filters the keys to find those whose corresponding value matches the current value
   a = myDictKeys.filter(key => myDict[key] === myDictValues[i]);
 
   // For each matched key, pushes it into the array associated with the current value
