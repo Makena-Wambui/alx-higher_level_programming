088560219dc800e5abcd1493481f3c65789288d5
iammakena@gmail.com <root@MAKENA>
2024-04-23 22:13:20 +0300

    Simple closure

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  // the converter function takes a single argument, base
+  // this following function is a  closure defined using a return statement.
+  // it takes an argument number. 
+  return function (number) {
+    // this closure converts number to a string using the specified base.
+    return number.toString(base);
+  };
+};
