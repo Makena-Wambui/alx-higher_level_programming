a1a39097dff93eed176eeb98f63cc9a1a4b8e5a4
Makena-Wambui <iammakena@gmail.com>
2024-05-12 17:11:57 +0300

    More changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,29 +13,29 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    # if all elements are the same
-    if all(x == list_of_integers[0] for x in list_of_integers):
-        return list_of_integers[0]
-    # the index of the first element in the array.
     low = 0
 
     # so we dont go out of bounds of the array
     n = len(list_of_integers) - 1
 
-    while low <= n:
+    while low < n:
         # define our mid point
         mid = (low + n) // 2
 
-        # element to the left of mid
-        mid_l = list_of_integers[mid - 1] if mid - 1 > 0 else float("-inf")
-
-        # element to the right of mid
-        mid_r = list_of_integers[mid + 1] \
-            if mid + 1 < len(list_of_integers) else float("inf")
-
-        if mid_l < list_of_integers[mid] and mid_r < list_of_integers[mid]:
+        # Check if mid is a peak
+        if (mid == 0 or
+                list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+           (mid == len(list_of_integers) - 1 or
+           list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
-        elif mid_l > list_of_integers[mid]:
-            n = mid - 1
-        else:
+
+        # If the element to the right of mid is greater, peak is to the right
+        elif mid < len(list_of_integers) - 1 and \
+                list_of_integers[mid] < list_of_integers[mid + 1]:
             low = mid + 1
+
+        # Otherwise, peak is to the left
+        else:
+            n = mid
+    # Return the remaining element if it's a peak
+    return list_of_integers[low]
