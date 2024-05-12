c9aa1e9beecd98ac25b76df51f6ea56c2fed5a2f
Makena-Wambui <iammakena@gmail.com>
2024-05-12 16:29:44 +0300

    Changed algo to binary search from linear search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,10 +13,29 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
+    # if all elements are the same
+    if all(x == list_of_integers[0] for x in list_of_integers):
+        return list_of_integers[0]
+    # the index of the first element in the array.
+    low = 0
+
+    # so we dont go out of bounds of the array
     n = len(list_of_integers) - 1
 
-    for i in range(0, n):
-        if list_of_integers[i] > list_of_integers[i + 1]:
-            return list_of_integers[i]
+    while low <= n:
+        # define our mid point
+        mid = (low + n) // 2
 
-    return list_of_integers[n]
+        # element to the left of mid
+        mid_l = list_of_integers[mid - 1] if mid - 1 > 0 else float("-inf")
+
+        # element to the right of mid
+        mid_r = list_of_integers[mid + 1] \
+            if mid + 1 < len(list_of_integers) else float("inf")
+
+        if mid_l < list_of_integers[mid] and mid_r < list_of_integers[mid]:
+            return list_of_integers[mid]
+        elif mid_l > list_of_integers[mid]:
+            n = mid - 1
+        else:
+            low = mid + 1
