a19b9231c1b3d64c5220a220539397b08199da71
Makena-Wambui <iammakena@gmail.com>
2024-05-13 13:08:20 +0300

    Added if __name__ == __main__

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,14 +12,15 @@ import sys
 import urllib.request
 
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-# create a Request object
-request = urllib.request.Request(url)
+    # create a Request object
+    request = urllib.request.Request(url)
 
-# call urlopen on request
-with urllib.request.urlopen(request) as response:
-    # access the response headers.
-    # use dict to convert into a dictionary object.
-    # use get to extract the value of the X-Request-Id key.
-    print(dict(response.headers).get("X-Request-Id"))
+    # call urlopen on request
+    with urllib.request.urlopen(request) as response:
+        # access the response headers.
+        # use dict to convert into a dictionary object.
+        # use get to extract the value of the X-Request-Id key.
+        print(dict(response.headers).get("X-Request-Id"))
