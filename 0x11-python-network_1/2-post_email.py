8c7e3ca1ac0bb6c4bc939b901379c4122cb67995
Makena-Wambui <iammakena@gmail.com>
2024-05-13 13:37:41 +0300

    POSTing data upto the server using urllib

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+This module takes in a URL, and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+The email must be sent in the email variable.
+
+You must use the packages urllib and sys
+You are not allow to import packages other than urllib and sys.
+You must use a with statement.
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email_val = str(sys.argv[2])
+    email = {"email": "email_val"}
+
+    # first encode the email using the urlencode function
+    email = urllib.parse.urlencode(email)
+    data = email.encode("ascii")
+
+    # create a Request object, pass the email/data as a param.
+    request = urllib.request.Request(url, data)
+
+    # call urlopen on request
+    with urllib.request.urlopen(request) as response:
+        response = respons.read()
+        response = response.decode("utf-8")
+        print(response)
