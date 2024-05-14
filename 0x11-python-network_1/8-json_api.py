c3368bc45a5eeb22b4b8caf6c5145cb91959b92f
Makena-Wambui <iammakena@gmail.com>
2024-05-14 04:25:19 -0700

    Removed print statement from except clause

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -33,4 +33,3 @@ if __name__ == "__main__":
             print("[{}] {}".format(response.get('id'), response.get('name')))
     except ValueError:
         print("Not a valid JSON")
-        print(responese.content)
