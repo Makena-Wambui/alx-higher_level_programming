cc5e826e487f4bc9b0621c29437467129e32d752
Makena-Wambui <iammakena@gmail.com>
2024-07-05 22:04:45 +0300

    displaying the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,27 +13,20 @@ You must use a with statement.
 
 import sys
 import urllib.request
-import urllib.parse
-import urllib.error
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email_val = sys.argv[2]
-    email = {"email": "email_val"}
 
-    # first encode the email using the urlencode function
-    email = urllib.parse.urlencode(email)
-    data = email.encode("ascii")
+    # Create a Request object
+    request = urllib.request.Request(url)
 
-    # create a Request object, pass the email/data as a param.
-    request = urllib.request.Request(url, data)
+    # Call urlopen on the request and handle the response
+    with urllib.request.urlopen(request) as response:
+        # Get all headers from the response
+        headers = response.getheaders()
 
-    # call urlopen on request
-    try:
-        with urllib.request.urlopen(request) as response:
-            response = response.read()
-            response = response.decode("utf-8")
-            print(response)
-    except urllib.error.URLError:
-        pass
+        # Extract the X-Request-Id header
+        for header in headers:
+            if header[0].lower() == 'x-request-id':
+                print(header[1])
+                break
